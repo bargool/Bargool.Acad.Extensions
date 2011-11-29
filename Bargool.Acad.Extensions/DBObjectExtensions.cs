13386f0d6c0d0e47c5eb1a9edfcdc8a09b0e81b5
bargool <bargool@gmail.com>
2011-11-29 17:23:23 +0400

    Написал ReadXrecord и DeleteXrecord

--- Bargool.Acad.Extensions/DBObjectExtensions.cs
+++ Bargool.Acad.Extensions/DBObjectExtensions.cs
@@ -12,20 +12,23 @@ using Autodesk.AutoCAD.DatabaseServices;
 namespace Bargool.Acad.Extensions
 {
 	/// <summary>
-	/// Класс расширений DBObject
+	/// Класс расширений для DBObject
 	/// </summary>
 	public static class DBObjectExtensions
 	{
+		
 		/// <summary>
 		/// Добавляет Xrecord к объекту
 		/// </summary>
-		/// <param name="o">Объект для добавления</param>
+		/// <param name="o">Объект для добавления, куда добавляется Xrecord</param>
 		/// <param name="xrecordName">Имя записи</param>
-		/// <param name="buffer">Данные для записи</param>
-		/// <param name="rewrite">Перезаписывать Xrecord? Если false - будем добавлять</param>
+		/// <param name="buffer">Данные для добавления</param>
+		/// <param name="rewrite">Перезаписывать Xrecord? Если false - будем добавлять в конец</param>
 		/// <param name="addDuplicates">Добавлять дубликаты? Если false - дубликаты не добавлять</param>
 		public static void WriteXrecord(this DBObject o, string xrecordName, ResultBuffer buffer, bool rewrite, bool addDuplicates)
 		{
+			if (xrecordName == null)
+				throw new ArgumentNullException("xrecordName is null");
 			if (buffer == null)
 				throw new ArgumentNullException("buffer is null");
 			if (o.ExtensionDictionary==ObjectId.Null || o.ExtensionDictionary.IsErased) {
@@ -39,6 +42,7 @@ namespace Bargool.Acad.Extensions
 				}
 				else {
 					dict.SetAt(xrecordName, xrecord);
+					o.Database.TransactionManager.AddNewlyCreatedDBObject(o, true);
 				}
 				if (rewrite) {
 					xrecord.Data = buffer;
@@ -54,6 +58,58 @@ namespace Bargool.Acad.Extensions
 						xrecord.Data = new ResultBuffer(tempBuffer
 						                                .Union(buffer.AsArray())
 						                                .ToArray());
+					}
+				}
+			}
+		}
+		
+		/// <summary>
+		/// Возвращает содержимое Xrecord
+		/// </summary>
+		/// <param name="o">Объект, из которого извлекается содержимое Xrecord</param>
+		/// <param name="xrecordName">Имя записи</param>
+		/// <returns>Содержимое Xrecord, либо null, если такого Xrecord нет</returns>
+		public static ResultBuffer GetXrecord(this DBObject o, string xrecordName)
+		{
+			if (xrecordName == null)
+				throw new ArgumentNullException("xrecordName is null");
+			if (o.ExtensionDictionary != ObjectId.Null &&
+			   !o.ExtensionDictionary.IsErased)
+			{
+				using (DBDictionary dict = (DBDictionary)o.ExtensionDictionary.GetObject(OpenMode.ForRead))
+				{
+					if (dict.Contains(xrecordName))
+					{
+						Xrecord xrecord = dict.GetAt(xrecordName).GetObject(OpenMode.ForRead) as Xrecord;
+						if (xrecord != null)
+							return xrecord.Data;
+					}
+				}
+			}
+			return null;
+		}
+		
+		/// <summary>
+		/// Удаляет Xrecord с заданным именем, если после удаления остается пустой словарь - он также удаляется
+		/// </summary>
+		/// <param name="o">Объект, у которого удаляется Xrecord</param>
+		/// <param name="xrecordName">Имя записи</param>
+		public static void DeleteXrecord(this DBObject o, string xrecordName)
+		{
+			if (xrecordName == null)
+				throw new ArgumentNullException("xrecordName is null");
+			if (o.ExtensionDictionary != ObjectId.Null &&
+			   !o.ExtensionDictionary.IsErased)
+			{
+				using (DBDictionary dict = (DBDictionary)o.ExtensionDictionary.GetObject(OpenMode.ForRead))
+				{
+					if (dict.Contains(xrecordName))
+					{
+					    dict.Remove(xrecordName);
+					    if (dict.Count==0)
+					    {
+					    	dict.Erase();
+					    }
 					}
 				}
 			}
