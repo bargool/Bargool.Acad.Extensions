77ac21132cb387b1b9a45246ada6d3796ca7ff6a
bargool <bargool@gmail.com>
2012-01-31 18:33:05 +0400

    Добавил расширение для получения имени BlockReference

---
+++ Bargool.Acad.Extensions/BlockReferenceExtensions.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * User: aleksey.nakoryakov
+ * Date: 31.01.2012
+ * Time: 17:37
+ */
+using System;
+
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace Bargool.Acad.Extensions
+{
+	/// <summary>
+	/// Класс расширений для BlockReference
+	/// </summary>
+	public static class BlockReferenceExtensions
+	{
+		public static string GetBlockName(this BlockReference o)
+		{
+//			if (o.IsDynamicBlock)
+//			{
+				BlockTableRecord btr = (BlockTableRecord)o.DynamicBlockTableRecord.GetObject(OpenMode.ForRead);
+				return btr.Name;
+//			}
+//			else
+//			{
+//				return o.Name;
+//			}
+		}
+	}
+}
