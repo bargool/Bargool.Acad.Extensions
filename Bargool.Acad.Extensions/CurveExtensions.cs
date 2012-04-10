81658dea9f4d7b4e23c550af0f0211ff188385ad
bargool <bargool@gmail.com>
2012-04-10 18:58:32 +0400

    Добавил файл для методов расширения класса Curve

---
+++ Bargool.Acad.Extensions/CurveExtensions.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * User: aleksey.nakoryakov
+ * Date: 10.04.12
+ * Time: 18:10
+ */
+using System;
+
+namespace Bargool.Acad.Extensions
+{
+	/// <summary>
+	/// Description of CurveExtensions.
+	/// </summary>
+	public static class CurveExtensions
+	{
+		public CurveExtensions()
+		{
+		}
+	}
+}
