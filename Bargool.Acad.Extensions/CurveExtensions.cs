233e155b939a19d6fe88858987930e7fef13ed1e
bargool <bargool@gmail.com>
2012-04-11 11:53:38 +0400

    Добавил метод для определения, лежит ли точка на кривой

--- Bargool.Acad.Extensions/CurveExtensions.cs
+++ Bargool.Acad.Extensions/CurveExtensions.cs
@@ -4,16 +4,33 @@
  * Time: 18:10
  */
 using System;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
 
 namespace Bargool.Acad.Extensions
 {
 	/// <summary>
-	/// Description of CurveExtensions.
+	/// Методы расширения для класса Curve
 	/// </summary>
 	public static class CurveExtensions
 	{
-		public CurveExtensions()
+		/// <summary>
+		/// Метод определяет лежит ли точка на указанной кривой. Точка считается на кривой, если расстояние от неё до кривой не больше,
+		/// чем значение Tolerance в данном чертеже
+		/// </summary>
+		/// <param name="curve">Кривая, на которой определяем точку</param>
+		/// <param name="point">Точка, положение которой определяем</param>
+		/// <returns>Если расстояние от данной точки до кривой меньше Tolerance, возвращает true</returns>
+		public static bool IsPointOnCurve(this Curve curve, Point3d point)
 		{
+			try
+			{
+				Point3d pt = curve.GetClosestPointTo(point, false);
+				return (pt - point).Length <= Tolerance.Global.EqualPoint;
+			}
+			catch 
+			{ }
+			return false;
 		}
 	}
 }
