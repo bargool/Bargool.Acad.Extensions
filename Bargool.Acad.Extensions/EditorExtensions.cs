40803a300d9883f214372e8e62b740e7ac4f13d2
bargool <bargool@gmail.com>
2014-02-01 09:49:41 +0400

    Добавил класс расширений для Editor. Добавил туда метод написания строки

---
+++ Bargool.Acad.Extensions/EditorExtensions.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * User: aleksey
+ * Date: 01.02.2014
+ * Time: 9:35
+ */
+using System;
+using System.Text;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace Bargool.Acad.Extensions
+{
+	/// <summary>
+	/// Description of EditorExtensions.
+	/// </summary>
+	public static class EditorExtensions
+	{
+		public static void WriteLine(this Editor ed, string message, params object[] parameters)
+		{
+			string s = string.Format(message, parameters);
+			ed.WriteMessage("\n" + s + "\n");
+		}
+	}
+}
