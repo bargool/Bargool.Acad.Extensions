331de2ffebfd13e61dbc1878c823b1daf0853fa2
Alexey Nakoryakov <alexey.nakoryakov@gms.ru>
2014-02-10 12:15:51 +0400

    Добавил класс расширений запросов. Сделал расширение для получения ObjectId выделенных пользователем объектов

---
+++ Bargool.Acad.Extensions/PromptExtensions.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alexey.nakoryakov
+ * Date: 10.02.2014
+ * Time: 12:12
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace Bargool.Acad.Extensions
+{
+	/// <summary>
+	/// Description of PromptExtensions.
+	/// </summary>
+	public static class PromptExtensions
+	{
+		public static IEnumerable<ObjectId> GetSelectedObjectIds(this PromptSelectionResult res)
+		{
+			return res.Value.Cast<SelectedObject>().Select(so => so.ObjectId);
+		}
+	}
+}
