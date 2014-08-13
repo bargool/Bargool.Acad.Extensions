8a9d4f7ed21fadf26d07d10ea4d8caa88e7a9583
Alexey Nakoryakov <alexey.nakoryakov@gms.ru>
2014-08-13 15:02:43 +0400

    Небольшие добавления и обновления

--- Bargool.Acad.Extensions/EditorExtensions.cs
+++ Bargool.Acad.Extensions/EditorExtensions.cs
@@ -14,85 +14,99 @@ using Autodesk.AutoCAD.Geometry;
 
 namespace Bargool.Acad.Extensions
 {
-	/// <summary>
-	/// Description of EditorExtensions.
-	/// </summary>
-	public static class EditorExtensions
-	{
-		public static void WriteLine(this Editor ed, string message, params object[] parameters)
-		{
-			string s = string.Format(message, parameters);
-			ed.WriteMessage("\n" + s + "\n");
-		}
-		
-		/// <summary>
-		/// Borrowed from http://www.theswamp.org/index.php?topic=46442.msg514605#msg514605
-		/// </summary>
-		public static void Zoom(this Editor ed, Extents3d ext)
-		{
-			if (ed == null)
-				throw new ArgumentNullException("ed");
-			
-			using (ViewTableRecord view = ed.GetCurrentView())
-			{
-				ext.TransformBy(view.WorldToEye());
-				view.Width = ext.MaxPoint.X - ext.MinPoint.X;
-				view.Height = ext.MaxPoint.Y - ext.MinPoint.Y;
-				view.CenterPoint = new Point2d(
-					(ext.MaxPoint.X + ext.MinPoint.X) / 2.0,
-					(ext.MaxPoint.Y + ext.MinPoint.Y) / 2.0);
-				ed.SetCurrentView(view);
-			}
-		}
-		
-		/// <summary>
-		/// Borrowed from http://www.theswamp.org/index.php?topic=46442.msg514605#msg514605
-		/// </summary>
-		/// <param name="ed"></param>
-		public static void ZoomExtents(this Editor ed)
-		{
-			if (ed == null)
-				throw new ArgumentNullException("ed");
-			
-			Database db = ed.Document.Database;
-			Extents3d ext = (short)Application.GetSystemVariable("cvport") == 1 ?
-				new Extents3d(db.Pextmin, db.Pextmax) :
-				new Extents3d(db.Extmin, db.Extmax);
-			ed.Zoom(ext);
-		}
-		
-		/// <summary>
-		/// Returns the transformation matrix from the ViewportTableRecord DCS to WCS.
-		/// </summary>
-		/// <remarks>Borrowed from http://www.acadnetwork.com/index.php?topic=232.msg406#msg406</remarks>
-		/// <param name="view">The ViewportTableRecord instance this method applies to.</param>
-		/// <returns>The DCS to WCS transformation matrix.</returns>
-		public static Matrix3d EyeToWorld(this AbstractViewTableRecord view)
-		{
-			return
-				Matrix3d.Rotation(-view.ViewTwist, view.ViewDirection, view.Target) *
-				Matrix3d.Displacement(view.Target - Point3d.Origin) *
-				Matrix3d.PlaneToWorld(view.ViewDirection);
-		}
+    /// <summary>
+    /// Description of EditorExtensions.
+    /// </summary>
+    public static class EditorExtensions
+    {
+        public static void WriteLine(this Editor ed, string message, params object[] parameters)
+        {
+            string s = string.Format(message, parameters);
+            ed.WriteMessage("\n" + s + "\n");
+        }
 
-		/// <summary>
-		/// Returns the transformation matrix from the ViewportTableRecord WCS to DCS.
-		/// </summary>
-		/// <remarks>Borrowed from http://www.acadnetwork.com/index.php?topic=232.msg406#msg406</remarks>
-		/// <param name="view">The ViewportTableRecord instance this method applies to.</param>
-		/// <returns>The WCS to DCS transformation matrix.</returns>
-		public static Matrix3d WorldToEye(this AbstractViewTableRecord view)
-		{
-			return view.EyeToWorld().Inverse();
-		}
+        /// <summary>
+        /// Borrowed from http://www.theswamp.org/index.php?topic=46442.msg514605#msg514605
+        /// </summary>
+        public static void Zoom(this Editor ed, Extents3d ext)
+        {
+            if (ed == null)
+                throw new ArgumentNullException("ed");
 
+            using (ViewTableRecord view = ed.GetCurrentView())
+            {
+                ext.TransformBy(view.WorldToEye());
+                view.Width = ext.MaxPoint.X - ext.MinPoint.X;
+                view.Height = ext.MaxPoint.Y - ext.MinPoint.Y;
+                view.CenterPoint = new Point2d(
+                    (ext.MaxPoint.X + ext.MinPoint.X) / 2.0,
+                    (ext.MaxPoint.Y + ext.MinPoint.Y) / 2.0);
+                ed.SetCurrentView(view);
+            }
+        }
+
+        /// <summary>
+        /// Borrowed from http://www.theswamp.org/index.php?topic=46442.msg514605#msg514605
+        /// </summary>
+        /// <param name="ed"></param>
+        public static void ZoomExtents(this Editor ed)
+        {
+            if (ed == null)
+                throw new ArgumentNullException("ed");
+
+            Database db = ed.Document.Database;
+            Extents3d ext = (short)Application.GetSystemVariable("cvport") == 1 ?
+                new Extents3d(db.Pextmin, db.Pextmax) :
+                new Extents3d(db.Extmin, db.Extmax);
+            ed.Zoom(ext);
+        }
+
+        /// <summary>
+        /// Returns the transformation matrix from the ViewportTableRecord DCS to WCS.
+        /// </summary>
+        /// <remarks>Borrowed from http://www.acadnetwork.com/index.php?topic=232.msg406#msg406</remarks>
+        /// <param name="view">The ViewportTableRecord instance this method applies to.</param>
+        /// <returns>The DCS to WCS transformation matrix.</returns>
+        public static Matrix3d EyeToWorld(this AbstractViewTableRecord view)
+        {
+            return
+                Matrix3d.Rotation(-view.ViewTwist, view.ViewDirection, view.Target) *
+                Matrix3d.Displacement(view.Target - Point3d.Origin) *
+                Matrix3d.PlaneToWorld(view.ViewDirection);
+        }
+
+        /// <summary>
+        /// Returns the transformation matrix from the ViewportTableRecord WCS to DCS.
+        /// </summary>
+        /// <remarks>Borrowed from http://www.acadnetwork.com/index.php?topic=232.msg406#msg406</remarks>
+        /// <param name="view">The ViewportTableRecord instance this method applies to.</param>
+        /// <returns>The WCS to DCS transformation matrix.</returns>
+        public static Matrix3d WorldToEye(this AbstractViewTableRecord view)
+        {
+            return view.EyeToWorld().Inverse();
+        }
+
+        // поле для использования в обработчике собятия
         private static Func<ObjectId, bool> _filterObjectMethod;
 
+        /// <summary>
+        /// Получение выделенных объектов. При этом учитывается возможность предварительного выбора объектов
+        /// </summary>
+        /// <param name="ed"></param>
+        /// <param name="pso"></param>
+        /// <returns></returns>
         public static PromptSelectionResult GetFilteredSelection(this Editor ed, PromptSelectionOptions pso)
         {
             return GetFilteredSelection(ed, pso, id => true);
         }
-
+        
+        /// <summary>
+        /// Получение выделенных объектов с учетом заданного фильтра. При этом учитывается возможность предварительного выбора объектов. 
+        /// </summary>
+        /// <param name="ed"></param>
+        /// <param name="pso"></param>
+        /// <param name="filterObjectMethod">Метод, по которому происходит фильтрация объектов</param>
+        /// <returns></returns>
         public static PromptSelectionResult GetFilteredSelection(this Editor ed, PromptSelectionOptions pso, Func<ObjectId, bool> filterObjectMethod)
         {
             _filterObjectMethod = filterObjectMethod;
@@ -102,10 +116,10 @@ namespace Bargool.Acad.Extensions
 
             if (res.Status == PromptStatus.OK)
             {
-                var filteredObjects = res.Value.GetObjectIds().Where(id => _filterObjectMethod(id));
+                var filteredObjects = res.GetSelectedObjectIds().Where(id => _filterObjectMethod(id));
                 ed.SetImpliedSelection(filteredObjects.ToArray());
                 res = ed.SelectImplied();
-                ed.SetImpliedSelection(new ObjectId[0]);
+                //ed.SetImpliedSelection(new ObjectId[0]);
             }
             else
             {
@@ -115,9 +129,15 @@ namespace Bargool.Acad.Extensions
                 ed.SelectionAdded -= selHandler;
             }
 
+            _filterObjectMethod = null;
             return res;
         }
 
+        /// <summary>
+        /// Обработчик события для применения фильтрации при выборе объектов
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private static void ed_SelectionAdded(object sender, SelectionAddedEventArgs e)
         {
             ObjectId[] ids = e.AddedObjects.GetObjectIds();
@@ -128,5 +148,5 @@ namespace Bargool.Acad.Extensions
                     e.Remove(i);
             }
         }
-	}
+    }
 }
---
+++ Bargool.Acad.Extensions/LayoutManagerExtensions.cs
@@ -0,0 +1,104 @@
+﻿// © Andrey Bushman, 2013
+// From http://adn-cis.org/forum/index.php?topic=411.0
+//Microsoft
+using System;
+
+//Autodesk
+using cad = Autodesk.AutoCAD.ApplicationServices.Application;
+using App = Autodesk.AutoCAD.ApplicationServices;
+using Db = Autodesk.AutoCAD.DatabaseServices;
+using Ed = Autodesk.AutoCAD.EditorInput;
+
+namespace Bargool.Acad.Extensions
+{
+
+    public static class LayoutManagerExtensionMethods
+    {
+        /// <summary>
+        /// This is Extension Method for the <c>Autodesk.AutoCAD.DatabaseServices.LayoutManager</c>
+        /// class. It gets the current space in the current Database.
+        /// </summary>
+        /// <param name="mng">Target <c>Autodesk.AutoCAD.DatabaseServices.LayoutManager</c>
+        /// instance.</param>
+        /// <returns>Returns the SpaceEnum value.</returns>            
+        public static SpaceEnum GetCurrentSpaceEnum(this Db.LayoutManager mng)
+        {
+            Db.Database db = cad.DocumentManager.MdiActiveDocument.Database;
+            Int16 tilemode = (Int16)cad.GetSystemVariable("TILEMODE");
+
+            if (tilemode == 1)
+                return SpaceEnum.Model;
+
+            Int16 cvport = (Int16)cad.GetSystemVariable("CVPORT");
+            if (cvport == 1)
+                return SpaceEnum.Layout;
+            else
+                return SpaceEnum.Viewport;
+        }
+
+        /// <summary>
+        /// This is Extension Method for the <c>Autodesk.AutoCAD.DatabaseServices.LayoutManager</c>
+        /// class. It gets the name of the current space in the current Database.
+        /// </summary>
+        /// <param name="mng">Target <c>Autodesk.AutoCAD.DatabaseServices.LayoutManager</c>
+        /// instance.</param>
+        /// <returns>Returns the name of current space.</returns>
+        public static String GetCurrentSpaceName(this Db.LayoutManager mng)
+        {
+            SpaceEnum space = GetCurrentSpaceEnum(mng);
+            Db.Database db = cad.DocumentManager.MdiActiveDocument.Database;
+            String modelSpaceLocalizedName = String.Empty;
+            using (Db.Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                Db.BlockTable bt = tr.GetObject(db.BlockTableId, Db.OpenMode.ForRead) as Db.BlockTable;
+                Db.BlockTableRecord btr = tr.GetObject(bt[Db.BlockTableRecord.ModelSpace], Db.OpenMode.ForRead)
+                        as Db.BlockTableRecord;
+                modelSpaceLocalizedName = (tr.GetObject(btr.LayoutId, Db.OpenMode.ForRead) as Db.Layout).LayoutName;
+            }
+            String result = space == SpaceEnum.Viewport ?
+                    "Model" as String : mng.CurrentLayout;
+            return result;
+        }
+
+        /// <summary>
+        /// This is Extension Method for the <c>Autodesk.AutoCAD.DatabaseServices.LayoutManager</c>
+        /// class. It gets the localized name of the Model tab.
+        /// </summary>
+        /// <param name="mng">Target <c>Autodesk.AutoCAD.DatabaseServices.LayoutManager</c>
+        /// instance.</param>
+        /// <returns>Returns the name of current space.</returns>
+        public static String GetModelTabLocalizedName(this Db.LayoutManager mng)
+        {
+            Db.Database db = cad.DocumentManager.MdiActiveDocument.Database;
+            String modelTabLocalizedName = String.Empty;
+            using (Db.Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                Db.BlockTable bt = tr.GetObject(db.BlockTableId, Db.OpenMode.ForRead) as Db.BlockTable;
+                Db.BlockTableRecord btr = tr.GetObject(bt[Db.BlockTableRecord.ModelSpace], Db.OpenMode.ForRead)
+                        as Db.BlockTableRecord;
+                modelTabLocalizedName = (tr.GetObject(btr.LayoutId, Db.OpenMode.ForRead) as Db.Layout).LayoutName;
+            }
+            return modelTabLocalizedName;
+        }
+    }
+
+
+    /// <summary>
+    /// This enum indicates the current space in the current Database.
+    /// </summary>
+    public enum SpaceEnum
+    {
+        /// <summary>
+        /// The Model space.
+        /// </summary>
+        Model,
+        /// <summary>
+        /// The Layout space.
+        /// </summary>
+        Layout,
+        /// <summary>
+        /// The Model space through the Layout's viewport.
+        /// </summary>
+        Viewport
+    }
+}
