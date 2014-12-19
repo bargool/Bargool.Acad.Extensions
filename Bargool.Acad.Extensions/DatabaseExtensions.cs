0a3dda6fb739e52be76d8c4acbd2ae1ea9e07f44
Alexey Nakoryakov <alexey.nakoryakov@gms.ru>
2014-12-19 10:55:44 +0300

    Сделал получение OId из базы по фильру

--- Bargool.Acad.Extensions/DatabaseExtensions.cs
+++ Bargool.Acad.Extensions/DatabaseExtensions.cs
@@ -11,59 +11,75 @@ using Autodesk.AutoCAD.DatabaseServices;
 
 namespace Bargool.Acad.Extensions
 {
-	/// <summary>
-	/// Description of DatabaseExtensions.
-	/// </summary>
-	public static class DatabaseExtensions
-	{
-//		public static Dictionary<string, List<ObjectId>> GetEntities(this Database db)
-//		{
-//			return db.GetEntities(false, false, false);
-//		}
-//
-//		public static Dictionary<string, List<ObjectId>> GetEntities(this Database db,
-//		                                                             bool EvalOffLayers,
-//		                                                             bool EvalFrozenLayers)
-//		{
-//			return db.GetEntities(EvalOffLayers, EvalFrozenLayers, false);
-//		}
-//		public static Dictionary<string, List<ObjectId>> GetEntities(this Database db,
-//		                                                             bool EvalOffLayers,
-//		                                                             bool EvalFrozenLayers,
-//		                                                             bool EvalAnonymBlocks)
-//		{
-//			return db.GetEntities(EvalOffLayers, EvalFrozenLayers, EvalAnonymBlocks, false);
-//		}
-		
-		public static Dictionary<string, List<ObjectId>> GetEntities(this Database db,
-		                                                             bool EvalOffLayers,
-		                                                             bool EvalFrozenLayers,
-		                                                             bool EvalAnonymBlocks,
-		                                                             bool EvalXrefs,
-		                                                             bool EvalLayouts)
-		{
-			Dictionary<string, List<ObjectId>> result = new Dictionary<string, List<ObjectId>>();
-			using (Transaction tr = db.TransactionManager.StartTransaction())
-			{
-				BlockTable bt = (BlockTable)tr.GetObject(db.BlockTableId, OpenMode.ForRead);
-				foreach (ObjectId btrId in bt)
-				{
-					BlockTableRecord btr = (BlockTableRecord)tr.GetObject(btrId, OpenMode.ForRead);
-					if (btr.IsFromExternalReference&&!EvalXrefs)
-						continue;
-					if (btr.IsAnonymous&&!EvalAnonymBlocks)
-						continue;
-					if (btr.IsLayout&&!EvalLayouts)
-						continue;
-					foreach (KeyValuePair<string, List<ObjectId>> kvp in btr.GetEntities(EvalOffLayers, EvalFrozenLayers))
-					{
-						if (!result.ContainsKey(kvp.Key))
-							result.Add(kvp.Key, new List<ObjectId>());
-						result[kvp.Key].AddRange(kvp.Value);
-					}
-				}
-			}
-			return result;
-		}
-	}
+    /// <summary>
+    /// Description of DatabaseExtensions.
+    /// </summary>
+    public static class DatabaseExtensions
+    {
+        //		public static Dictionary<string, List<ObjectId>> GetEntities(this Database db)
+        //		{
+        //			return db.GetEntities(false, false, false);
+        //		}
+        //
+        //		public static Dictionary<string, List<ObjectId>> GetEntities(this Database db,
+        //		                                                             bool EvalOffLayers,
+        //		                                                             bool EvalFrozenLayers)
+        //		{
+        //			return db.GetEntities(EvalOffLayers, EvalFrozenLayers, false);
+        //		}
+        //		public static Dictionary<string, List<ObjectId>> GetEntities(this Database db,
+        //		                                                             bool EvalOffLayers,
+        //		                                                             bool EvalFrozenLayers,
+        //		                                                             bool EvalAnonymBlocks)
+        //		{
+        //			return db.GetEntities(EvalOffLayers, EvalFrozenLayers, EvalAnonymBlocks, false);
+        //		}
+
+        public static Dictionary<string, List<ObjectId>> GetEntities(this Database db,
+                                                                     bool EvalOffLayers,
+                                                                     bool EvalFrozenLayers,
+                                                                     bool EvalAnonymBlocks,
+                                                                     bool EvalXrefs,
+                                                                     bool EvalLayouts)
+        {
+            Dictionary<string, List<ObjectId>> result = new Dictionary<string, List<ObjectId>>();
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                BlockTable bt = (BlockTable)tr.GetObject(db.BlockTableId, OpenMode.ForRead);
+                foreach (ObjectId btrId in bt)
+                {
+                    BlockTableRecord btr = (BlockTableRecord)tr.GetObject(btrId, OpenMode.ForRead);
+                    if (btr.IsFromExternalReference && !EvalXrefs)
+                        continue;
+                    if (btr.IsAnonymous && !EvalAnonymBlocks)
+                        continue;
+                    if (btr.IsLayout && !EvalLayouts)
+                        continue;
+                    foreach (KeyValuePair<string, List<ObjectId>> kvp in btr.GetEntities(EvalOffLayers, EvalFrozenLayers))
+                    {
+                        if (!result.ContainsKey(kvp.Key))
+                            result.Add(kvp.Key, new List<ObjectId>());
+                        result[kvp.Key].AddRange(kvp.Value);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static IEnumerable<ObjectId> FilterIntities(this Database db, Func<ObjectId, bool> filterFunction)
+        {
+            ObjectId result = ObjectId.Null;
+            foreach (Handle hnd in GenerateHandles(db))
+            {
+                if (db.TryGetObjectId(hnd, out result) && !result.IsNull && result.IsValid && !result.IsErased && !result.IsEffectivelyErased && filterFunction(result))
+                    yield return result;
+            }
+        }
+
+        static private IEnumerable<Handle> GenerateHandles(Database db)
+        {
+            for (Int64 i = db.BlockTableId.Handle.Value; i < db.Handseed.Value; i++)
+                yield return new Handle(i);
+        }
+    }
 }
